02ac778a8b5189b48446e9a4a66121a9dfb914ba
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-05-07 16:16:25 +0100

    app fully working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # A visitor can see the list of all restaurants = GET "restaurants"
-  resources :restaurants, only: [:index]
+#   A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  #  GET "restaurants/new"
+  # POST "restaurants"
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
